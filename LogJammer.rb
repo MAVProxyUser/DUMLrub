d42bb459dc8e0e5cdca13bbaeb774b19db29b9e4
Jan Dumon <jan@crossbar.net>
2017-08-25 19:28:26 +0200

    LogJammer: use correct IV
    By using the correct IV, the xor'ing trick of the first block is no
    longer required.

--- LogJammer.rb
+++ LogJammer.rb
@@ -17,27 +17,15 @@ begin
     encodedfile = ftp.getbinaryfile(filename,nil)
     cipher = OpenSSL::Cipher::AES128.new(:CBC)
     cipher.decrypt
-    cipher.key = "\x74\x68\x69\x73\x2d\x61\x65\x73\x2d\x6b\x65\x79\x00\x00\x00\x00"
-    cipher.iv  = "\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00"
+    cipher.key = "this-aes-key\x00\x00\x00\x00"
+    cipher.iv  = "0123456789abcdef"
     decrypted_plain_text = cipher.update(encodedfile) + cipher.final
-
-    # https://github.com/MAVProxyUser/DJI_ftpd_aes_unscramble/commit/93537fdec26435537399ea8595e0eee8725f5759
-    # undo the weird xor stuff that DJI does to try and beat us
-
-    puts "reversing odd DJI XOR bytes"
-    bytes = decrypted_plain_text.bytes.to_a
-    0.upto(9) do |i|
-        bytes[i] ^= 0x30 + i
-    end
-    10.upto(15) do |i|
-        bytes[i] ^= 0x57 + i
-    end
 
     # https://github.com/MAVProxyUser/DJI_ftpd_aes_unscramble/commit/73a21718c32ee2be96fc64b9b5acf033c5626176
     # Ruby uses PKCS7 Padding by default, so there's no need to adjust this
 
-    puts bytes.pack('c*')
-    File.open("upgrade00_logjam.txt", "w+") { |file| file.write(bytes.pack('c*')) }
+    puts decrypted_plain_text
+    File.open("upgrade00_logjam.txt", "w+") { |file| file.write(decrypted_plain_text) }
     puts "file written to upgrade00_logjam.txt"
 
 rescue Net::FTPPermError
