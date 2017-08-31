f75df4e749f989ce17b907de99524487f8f0d8b0
Jan Dumon <jan@crossbar.net>
2017-08-31 23:17:01 +0200

    LogJammer: moved the FTP stuff to a new generic class
    The bulk of LogJammer, responsible for fetching the upgrade log from the
    FTP server and decrypting it has been moved to a new file,
    PullFtpFile.rb as I needed the same functionality for fetching another
    file in an exploit.

--- LogJammer.rb
+++ LogJammer.rb
@@ -1,34 +1,15 @@
-require 'net/ftp'
-require "openssl"
-require "base64"
-include Base64
+#!/usr/bin/env ruby
+
+$:.unshift File.expand_path('.',__dir__)
+require 'PullFtpFile.rb'
 
 #puts "Please connect your DJI drone, verify the RNDIS is up, and press <enter>"
 #gets
 
-ftp = Net::FTP.new('192.168.42.2')
-ftp.passive = true
-ftp.login("LogJammer","IsDaRealest!" )
-puts "Logged into the FTPD"
-begin
-    filename = "/upgrade/dji/log/upgrade00.log"
-    puts "Snagging firmware update log file..."
-    puts "Downloading file: #{filename} to memory"
-    encodedfile = ftp.getbinaryfile(filename,nil)
-    cipher = OpenSSL::Cipher::AES128.new(:CBC)
-    cipher.decrypt
-    cipher.key = "this-aes-key\x00\x00\x00\x00"
-    cipher.iv  = "0123456789abcdef"
-    decrypted_plain_text = cipher.update(encodedfile) + cipher.final
+pullFtpFile = PullFtpFile.new
 
-    # https://github.com/MAVProxyUser/DJI_ftpd_aes_unscramble/commit/73a21718c32ee2be96fc64b9b5acf033c5626176
-    # Ruby uses PKCS7 Padding by default, so there's no need to adjust this
+log = pullFtpFile.get("/upgrade/dji/log/upgrade00.log")
+puts log 
+File.open("upgrade00_logjam.txt", "w+") { |file| file.write(log) }
+puts "file written to upgrade00_logjam.txt"
 
-    puts decrypted_plain_text
-    File.open("upgrade00_logjam.txt", "w+") { |file| file.write(decrypted_plain_text) }
-    puts "file written to upgrade00_logjam.txt"
-
-rescue Net::FTPPermError
-    puts "Weird FTP problem... unable to retrieve the upgrade00.log file"
-end
-ftp.close
