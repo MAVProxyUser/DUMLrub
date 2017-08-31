d30c071494639b168ae3d82e82afe75b7068fde5
Jan Dumon <jan@crossbar.net>
2017-08-31 23:38:51 +0200

    PullUpgradeLogs: made it more userfriendly
    When the only required command line argument (the serial port) is
    missing, it will exit with a helpful message.
    It also automatically finds the target device by probing the known
    target destinations (0x28 = AC, 0x2d = RC, 0x3c = Goggles)

--- PullUpgradeLogs.rb
+++ PullUpgradeLogs.rb
@@ -4,13 +4,31 @@ require 'rubygems'
 $:.unshift File.expand_path('.',__dir__)
 require 'DUML.rb'
 
-dst = 0x28 # Aircraft
-#dst = 0x2d # RC
+port = $*[0]
+if port == nil
+    puts "Usage: PullUpgradeLogs.rb <serial port>"
+    exit
+end
 
-con = DUML::ConnectionSerial.new("/dev/tty.usbmodem1425")
-@duml = DUML.new(0x2a, dst, con, 1.0, false)
-reply = @duml.cmd_common_get_cfg_file(2)
-File.open("upgrade_logs.tar.gz", "w+") { |file| file.write(reply) }
-puts "Logs written to upgrade_logs.tar.gz"
+dst = 0x00
+con = DUML::ConnectionSerial.new(port)
+@duml = DUML.new(0x2a, dst, con, 5.0, false)
+
+# Probe for the correct device
+[0x28, 0x2d, 0x3c].each do |d|
+    if @duml.cmd_dev_ping(0x2a, d, 0.05) != nil
+        dst = d
+        break
+    end
+end
+puts "dst = 0x%02x" % dst
+
+reply = @duml.cmd_common_get_cfg_file(2, 0x2a, dst)
+if reply && reply.length > 0
+    File.open("upgrade_logs.tar.gz", "w+") { |file| file.write(reply) }
+    puts "Logs written to upgrade_logs.tar.gz"
+else
+    puts "Failed to fetch the upgrade logs"
+end
 
 # vim: expandtab:ts=4:sw=4
